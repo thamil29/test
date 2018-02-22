de0a441fc355f47906334c42117402825036dd75
Thamilalagan V <thamil.1995@gmail.com>
2018-02-23 02:02:05 +0530

    loop modified

--- EmployeeData/src/com/database/Loops.java
+++ EmployeeData/src/com/database/Loops.java
@@ -6,7 +6,7 @@ public class Loops {
 		int age = 2;
 		if (age >= 20) {
 			System.out.println("You are an Teenager");
-		} else if (age > 36 || age > 45) {
+		} else if (age > 35 || age > 45) {
 			System.out.println("You are an adult");
 		} else {
 			System.out.println("You are child or senior citizen");
